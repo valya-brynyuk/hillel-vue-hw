9fe09287dc277407b270ed624df1a249970a942a
Bryniuk Valia <valyanychyporuk@gmail.com>
2024-06-01 02:38:31 +0300

    - completed

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script setup>
+import Card from "./components/Card.vue";
+</script>
+
+<template>
+  <Card title="Card title" button-text="Go somewhere">Some quick example text to build on the card</Card>
+</template>
+
---
+++ src/components/Card.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="card">
+    <div class="card-body">
+      <h4 class="card-title">{{ title }}</h4>
+      <p class="card-text">
+        <slot/>
+      </p>
+      <button type="button" class="btn btn-primary">{{ buttonText }}</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    buttonText: {
+      type: String,
+      required: true,
+    }
+  },
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
