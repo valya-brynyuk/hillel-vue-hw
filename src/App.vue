43ab537872e593339e7aa77e9b6970b44e8ecf17
Bryniuk Valia <valyanychyporuk@gmail.com>
2024-06-10 02:09:08 +0300

    - completed

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,15 @@
 <template>
-  <Definitions :data="definitions"/>
+  <Progress :percentage="90"/>
 </template>
 
 <script>
-import Definitions from "./components/Definitions.vue";
+
+import Progress from "./components/Progress.vue";
 
 export default {
   name: "App",
-  data() {
-    return {
-      definitions: [
-        {dt: 'one', dd: 'two', id: 1},
-        {dt: 'another term', dd: 'another description', id: 2},
-      ]
-    };
-  },
   components: {
-    Definitions
+    Progress
   }
 }
 
---
+++ src/components/Progress.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="progress">
+    <div
+        class="progress-bar"
+        role="progressbar"
+        :aria-valuenow="percentage"
+        aria-valuemin="0"
+        aria-valuemax="100"
+        aria-label="progressbar"
+        :style="style"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Progress",
+  props: {
+    percentage: {
+      type: Number,
+      required: true,
+    }
+  },
+  computed: {
+    style() {
+      return {
+        width: `${this.percentage}%`
+      };
+    }
+  }
+}
+</script>
